1bdae9707e48be107cac180a2acea72faf8f06d7
Justin <justinyeh13@gmail.com>
2025-03-24 16:28:45 -0400

    Added Logic to go back to main screen and to show the saving the product failed

--- A2_iOS_Justin_101400171/AddProductViewController.swift
+++ A2_iOS_Justin_101400171/AddProductViewController.swift
@@ -24,6 +24,22 @@ class AddProductViewController: UITableViewController {
             showAlert(message: "Please fill out all fields")
             return
         }
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let newProduct = Product(context: context)
+        
+        newProduct.productName = productName
+        newProduct.productDescription = productDescription
+        newProduct.productPrice = price
+        newProduct.productProvider = productProvider
+        
+        do {
+            try context.save()
+            navigationController?.popViewController(animated: true)
+        } catch {
+            print("Saving Product Failed: \(error)")
+            showAlert(message: "Saving Product Failed")
+        }
     }
     
     func showAlert(message: String) {
